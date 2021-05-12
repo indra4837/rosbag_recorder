a3a3e60bfcc66e5654fdbc038b94118823c60206
Indra Kurniawan <64070777+indra4837@users.noreply.github.com>
2021-05-12 22:30:43 +0800

    changed to relative path

--- scripts/rosbag_uploader.sh
+++ scripts/rosbag_uploader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source /opt/ros/melodic/setup.bash
-source /home/indra/catkin_ws/devel/setup.bash
+source ~/catkin_ws/devel/setup.bash
 
 while rostopic list ; do sleep 1; done
 onedrive --synchronize
